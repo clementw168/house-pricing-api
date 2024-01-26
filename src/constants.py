1a76f48952c66017b0ad404458c1b478ce7725bd
clementw168 <clementwang2001@gmail.com>
2024-01-26 17:12:08 +0100

    feat: dao init

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+# Data link
+DATA_LINK = "https://www.lri.fr/~gcharpia/deeppractice/2024/TP1/house_prices.csv"
+
+
+# Files
+
+DATA_FILE = "data/housing.csv"
+
+# Data parameters
+
+TARGET = "SalePrice"
+SPLIT_RATIO = 0.2
+RANDOM_STATE = 42
